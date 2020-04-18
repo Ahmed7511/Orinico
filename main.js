c4da0af11344677a03f608983c691044c188d539
Ahmed <sidouparadox7@gmail.com>
2020-04-18 21:49:21 +0200

    modif

--- main.js
+++ main.js
@@ -1,11 +1,33 @@
-var p1 = document.getElementByClassName('box1');
-p1.addEventListener('click',message1);
-p1.addEventListener('click',message2);
-
-function message1(){
-    this.innerHTML = 'salut';
-}
+      
  
-function message2(){
-    alert('message2');
-}
+ function main () {
+   window.fetch('http://localhost:3000/api/cameras')
+      .then(res => res.json())
+      .then(json => console.log(json ))
+ 
+ }
+  
+   function display(data){
+      const id = data.id;
+      const lentille = data.lenses;
+      const image = data.imageUrl;
+      const name = data.name;
+      const price = data.price;
+    const description = data.description;
+
+     document.getElementById('product').textContent = id;
+     document.getElementsByClassName('lentille').textContent = lentille;
+     document.getElementById('image').textContent = image;
+     document.getElementById('name').textContent = name;
+     document.getElementById('price').textContent = price;
+     document.getElementById('description').textContent = description;
+
+
+
+
+
+ }
+
+main();
+
+
