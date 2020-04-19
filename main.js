2709b4aaeb739b27e3e6ec75c7fd3bddfd4a8fc2
Ahmed <sidouparadox7@gmail.com>
2020-04-19 22:01:24 +0200

    debut page2

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ function main(){
  var request = new XMLHttpRequest();
    request.open('GET' , 'http://localhost:3000/api/cameras');
 
-   request.onload = function (){
+   request.onload = async function (){
 
     if(this.status == 200){
       var products = JSON.parse(request.responseText);
@@ -17,17 +17,19 @@ function main(){
                       '<li><strong>ID :</strong> ' + products[i]._id+'</li>' +
                       '<li><strong>Lentille :</strong> ' + products[i].lenses+'</li>' +
                       '<li><strong> name :</strong> ' + products[i].name+'</li>' +
-                      '<li><strong> price :</strong> ' + products[i].price+'</li>' +
+                      '<li><strong> price :</strong> ' + products[i].price/100 + ' €' +'</li>' +
                       '<li><strong> Description :</strong> ' + products[i].description+'</li>' +
+                      '<button class="btn" >Ajouter au panier</button> ' +
                     '</ul>' +
                     '</div>';
                     }
 
                     document.getElementById('products').innerHTML = output ;
 
-    }else{
+                  
 
-    }
+                    }
+
   };
 
    request.send();
