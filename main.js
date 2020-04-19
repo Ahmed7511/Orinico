a82d7756635fff8e3d3669095d26bfd5614e0106
Ahmed <sidouparadox7@gmail.com>
2020-04-19 15:55:56 +0200

    page1

--- main.js
+++ main.js
@@ -14,11 +14,11 @@ function main(){
         output += '<div class="product">'+
                  '<img src="' + products[i].imageUrl+'">' +
                  '<ul>' +
-                      '<li> ID :' + products[i]._id+'</li>' +
-                      '<li>Lentille :' + products[i].lenses+'</li>' +
-                      '<li> name :' + products[i].name+'</li>' +
-                      '<li> price:' + products[i].price+'</li>' +
-                      '<li> Description :' + products[i].description+'</li>' +
+                      '<li><strong>ID :</strong> ' + products[i]._id+'</li>' +
+                      '<li><strong>Lentille :</strong> ' + products[i].lenses+'</li>' +
+                      '<li><strong> name :</strong> ' + products[i].name+'</li>' +
+                      '<li><strong> price :</strong> ' + products[i].price+'</li>' +
+                      '<li><strong> Description :</strong> ' + products[i].description+'</li>' +
                     '</ul>' +
                     '</div>';
                     }
@@ -32,31 +32,6 @@ function main(){
 
    request.send();
  
-   
- 
- //function main () {
-   //window.fetch('http://localhost:3000/api/cameras/')
-   //.then(res => res.json())
-   //.then(data => console.log(data))
- 
- //}
-  
-   //function display(data){
-     //const id = data[0].id;
-     // const lentille = data.lenses;
-      //const image = data.imageUrl;
-      //const name = data[1].name;
-      //const price = data.price;
-    //const description = data.description;
-
-    //document.getElementById('product').textContent = id;
-     //document.getElementsByClassName('lentille').textContent = lentille;
-     //document.getElementById('image').textContent = image;
-     //document.getElementById('name').textContent = name;
-     //document.getElementById('price').textContent = price;
-     //document.getElementById('description').textContent = description;
-  
-    // console.log('name')
 }
 
 main();
