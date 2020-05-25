ebd335b26f6f5df7c6e7cd640608449e6438d077
Ahmed <sidouparadox7@gmail.com>
2020-05-25 22:43:16 +0200

    remplacement les appels jquery ajax par fetch

--- index.js
+++ index.js
@@ -1,38 +1,35 @@
+//------------------page index--------------//
+const urlAPI = "http://localhost:3000/api/cameras"; // Déclaration de l'Url de l'api
+const products = document.getElementById("products");
+const getProducts = async function () {          // création de la fonction asynchrone
+          let response = await fetch(urlAPI);         // on attend la réponse en json de l'API
+          let data = await response.json().then((data) => {  // une fois qu'on a la response en la declare : data
+                    data.forEach((product) => {
+                              products.innerHTML += `<div class="product">
+                                         <img src= "${product.imageUrl}"> 
+                                               <div>
+                                                     <ul> 
+                                                          <li id="price"><strong>Name:</strong>${product.name} </li>
+                                                          <li id="price"><strong>Prix:</strong> ${product.price / 100}</li>
+                                                          <li><strong>Description :</strong>${product.description}</li>
+                                                          '<button><a id ="btn" href="produit.html?id=${product._id }">personnalisez votre produit </a></button>
+                                                      </ul>
+                                                  </div>    
+                                             </div>`;
+                                             
+                                              });
+                                          });
+                            };
+      
+          getProducts();
 
-//------------------page index--------------// 
-$(function(){
-var $products = $('#products'); 
-  $.ajax({
-    url: 'http://localhost:3000/api/cameras',
-    type: 'GET',
-    typeData: 'json',
-    success: function(products){
-    $.each(products, function(i , product){            // i = index , product = item
-      $products.append('<div class="product">'+
-      '<img src="' + product.imageUrl+'">' +
-      '<div>' +
-      '<ul>' +
-      '<li><strong>Name :</strong>' +product.name +' </li>' +
-      '<li id="price"><strong>price:</strong>' +product.price/100+'€' +' </li>' +
-       '<li><strong>Description :</strong>' +product.description +' </li>' +
-      '<button><a id ="btn'+[i] +'" href="produit.html?id=' + product._id +'">personnalisez votre produit </a></button>' +
-      '</ul>' +
-       '</div>'
-     );
-    });
-    },  
-     error: function(){
-       console.log('bad request');
-     }
-  }) ;                                                  
-});
-function onloadCartNumbers(){
-  let cartItemsQuantityNumber = localStorage.getItem('totalCart');
-if(cartItemsQuantityNumber){
-  document.querySelector('.cart span').textContent = cartItemsQuantityNumber ; 
+function onloadCartNumbers() {
+          let cartItemsQuantityNumber = localStorage.getItem("totalCart");
+          if (cartItemsQuantityNumber) {
+                    document.querySelector(
+                              ".cart span"
+                    ).textContent = cartItemsQuantityNumber;
+          }
 }
 
-}
- 
 onloadCartNumbers();
-
--- panier.js
+++ panier.js
@@ -1,27 +1,32 @@
- let cartItems = localStorage.getItem("productInCart") || [];
-   cartItems = JSON.parse(cartItems);
-  let cartCost = localStorage.getItem("totalCost");
+let cartItems = localStorage.getItem("productInCart") || [];
+cartItems = JSON.parse(cartItems);
+let cartCost = localStorage.getItem("totalCost");
+function displayCart() {
+          let productsPanier = document.querySelector(".products-panier");
 
-  function displayCart(){
-  let productsPanier = document.querySelector('.products-panier');
-
-  if(cartItems && productsPanier){
-    productsPanier.innerHTML ='' ;
-    Object.values(cartItems).forEach(cartItem =>
-    productsPanier.innerHTML += `
+          if (cartItems && productsPanier) {
+                    productsPanier.innerHTML = "";
+                    Object.values(cartItems).forEach(
+                              (cartItem) =>
+                                        (productsPanier.innerHTML += `
                 <div class ="panier" >
                    <h5 class="name-product">${cartItem.name} </h5>  
-                    <h5 class="price-product"> ${cartItem.price/100},00 €  </h5>
-                    <h5 class="quantity-product"><i id="accre" class="far fa-plus-square"></i> ${cartItem.quantity} 
+                    <h5 class="price-product"> ${
+                              cartItem.price / 100
+                    },00 €  </h5>
+                    <h5 class="quantity-product"><i id="accre" class="far fa-plus-square"></i> ${
+                              cartItem.quantity
+                    } 
                      <i id="dec" class="fas fa-minus-circle"></i></h5>  
-                    <h5 class ="total-product" >${cartItem.price/100 * cartItem.quantity},00 € </h5>
+                    <h5 class ="total-product" >${
+                              (cartItem.price / 100) * cartItem.quantity
+                    },00 € </h5>
                 </div>   
 
-                `
+                `)
                     );
 
-
-       productsPanier.innerHTML += `
+                    productsPanier.innerHTML += `
          <div class = "totalContainer" >
           <div class = "totalTitle">total des produits</div>
           <h4 class = "totalProduct">
@@ -30,95 +35,86 @@
 
          </div>
 
-       `
-     
-
-  }
- }
-
- function onloadCartNumbers(){
-  let cartItemsQuantityNumber = localStorage.getItem('totalCart');
-if(cartItemsQuantityNumber){
-  document.querySelector('.cart span').textContent = cartItemsQuantityNumber ; 
+       `;
+          }
 }
 
+function onloadCartNumbers() {
+          let cartItemsQuantityNumber = localStorage.getItem("totalCart");
+          if (cartItemsQuantityNumber) {
+                    document.querySelector(
+                              ".cart span"
+                    ).textContent = cartItemsQuantityNumber;
+          }
 }
- 
+
 onloadCartNumbers();
-  
+
 displayCart();
 
-
-removeBtn = document.getElementsByClassName('remove');
-for( var i=0; i<removeBtn.length; i++){
-  var button = removeBtn[i]
-button.addEventListener('click', function(event){
-  alert('vous pouvez retourner vers la page d\'acceuil et choisir le produit que vous plaira !! ')
-  var buttonClicked = event.target
-  buttonClicked.parentElement.parentElement.remove()
-  localStorage.clear();
-  window.location.reload()
-});
+removeBtn = document.getElementsByClassName("remove");
+for (var i = 0; i < removeBtn.length; i++) {
+          var button = removeBtn[i];
+          button.addEventListener("click", function (event) {
+                    alert(
+                              "vous pouvez retourner vers la page d'acceuil et choisir le produit que vous plaira !! "
+                    );
+                    var buttonClicked = event.target;
+                    buttonClicked.parentElement.parentElement.remove();
+                    localStorage.clear();
+                    window.location.reload();
+          });
 }
 
+document.getElementById("formulaire").addEventListener("submit", function (e) {
+          var inputs = document.getElementsByTagName("input"); // on recupére tous les inputs
+          for (var i = 0; i < inputs.length; i++) {
+                    if (!inputs[i].value) {
+                              //  if inputs.value = '' ;
+                              error = "veuillez renseigner tous les champs !";
+                    } else if (inputs[i].value) {
+                              e.preventDefault(); // stopper le comporetement normale
+                              productsOrder();
+                    }
+          }
+});
 
-document.getElementById('formulaire').addEventListener('submit', function(e){
-  var inputs = document.getElementsByTagName('input');       // on recupére tous les inputs 
-  for(var i=0; i< inputs.length; i++){
-    if(!inputs[i].value){                               //  if inputs.value = '' ; 
-      error = "veuillez renseigner tous les champs !"
-    }else if (inputs[i].value){
-      (e).preventDefault();                   // stopper le comporetement normale
-      productsOrder();
-    }
+const productsOrder = async function () {
+          let firstName = document.getElementById("firstName").value;
+          let lastName = document.getElementById("lastName").value;
+          let address = document.getElementById("address").value;
+          let city = document.getElementById("city").value;
+          let email = document.getElementById("mail").value;
+          var contact = { firstName, lastName, address, city, email }; // on mis tt les inputs dans un objet
+          let products = [];
+          cartItems.forEach((cartItem) => {
+                    products.push(cartItem.id);
+          });
 
-  }
-})
+          let responseData = await fetch(
+                    "http://localhost:3000/api/cameras/order",
+                    {
+                              method: "post",
+                              headers: { "content-type": "application/json" }, // pour que  fetch comprendre qu'on va envoyer du json
+                              body: JSON.stringify({ products, contact }), // envoyer le body en json
+                    }
+          )
+                    .then((response) => {
+                              return response.json();
+                    })
+                    .then((data) => {
+                              let getOrderId = data.orderId;
+                              let getCartCost = cartCost;
+                              let orderRecap = { getOrderId, getCartCost };
 
+                              localStorage.clear();
+                              localStorage.setItem(
+                                        "orderRecap",
+                                        JSON.stringify(orderRecap)
+                              ); // on créé un localStorage qui contient le recap
 
-function productsOrder(){
-  
-  let firstName = document.getElementById('firstName').value;
-  let lastName = document.getElementById('lastName').value;
-  let address = document.getElementById('address').value;
-  let city = document.getElementById('city').value;
-  let email = document.getElementById('mail').value;
-  var contact = { firstName, lastName, address, city, email }; // on mis tt les inputs dans un objet 
-  let products = []
-  cartItems.forEach(cartItem =>{
-    products.push(cartItem.id)
-  })
+                              window.location = "confirmation.html";
+                    })
 
-    fetch('http://localhost:3000/api/cameras/order',
-    {method: 'post',
-    headers: {'content-type':'application/json'}, // pour que  fetch comprendre qu'on va envoyer du json 
-    body: JSON.stringify({products,contact})      // envoyer le body en json 
-    })
-      .then(response => { return response.json()})
-      .then(data => {
-        //console.log(data)
-        //console.log(data.orderId)
-        //console.log(cartCost)
-       
-        let getOrderId = data.orderId ;
-        let getCartCost = cartCost ;
-        let orderRecap = {getOrderId, getCartCost}
-        
-        
-        localStorage.clear();
-        localStorage.setItem('orderRecap', JSON.stringify(orderRecap)) // on créé un localStorage qui contient le recap 
-      
-    
-           
-       window.location = 'confirmation.html' ; 
-
-           
-      })
-      
-
-          .catch( Error => console.error('erreur'))
-
-
-    }
-    
-
+                    .catch((Error) => console.error("erreur"));
+};
